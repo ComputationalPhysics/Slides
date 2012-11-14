b73a57e6c523f5c1803bfc32ae7dcb0667b01880
Jadzia626 <code@jadzia626.net>
2012-11-14 15:14:46 +0100

    Minor fix

--- config_sample.php
+++ config_sample.php
@@ -1,7 +1,7 @@
 <?php
 
     // Main Congiguration
-    $cStyle    = "gray"; // Style of the index page
+    $cStyle    = "grey"; // Style of the index page [grey/blue]
     $cRoot     = "";     // The full URL for the location where you put this package
     $cPassword = "";     // Your pasword. Leave blank to disable
 
